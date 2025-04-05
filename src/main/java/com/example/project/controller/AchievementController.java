038a3bafbf6d9b9215ddc1db14614f408104d38d
pichet <good100vichet@gmail.com>
2025-04-05 17:16:27 +0700

    update user id

--- src/main/java/com/example/project/controller/AchievementController.java
+++ src/main/java/com/example/project/controller/AchievementController.java
@@ -8,6 +8,8 @@ import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +28,9 @@ public class AchievementController {
     @GetMapping
     public ResponseEntity<ApiResponse<List<Achievement>>> getAchievements(@RequestParam(defaultValue = "1") @Min(value = 1, message = "must be greater than 0") Integer page, @RequestParam(defaultValue = "10") Integer size) {
         List<Achievement> achievements = achievementService.getAllAchievement(page, size);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userId = (String) auth.getCredentials();
+
         ApiResponse<List<Achievement>> response = ApiResponse.<List<Achievement>>builder()
                 .success(true)
                 .message("Achievements retrieved successfully!")
